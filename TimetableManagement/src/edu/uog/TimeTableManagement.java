cad6584479203a472c9b71e7f12d8bd6275dd689
Nauman Riaz Chaudhry <nauman.riaz@gmail.com>
2020-07-03 09:32:07 +0500

    Project Changes

---
+++ TimetableManagement/src/edu/uog/TimeTableManagement.java
@@ -0,0 +1,78 @@
+package edu.uog;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import edu.uog.campus.Room;
+import edu.uog.campus.roomData;
+
+import edu.uog.campus.roomInterface;
+import edu.uog.course.Course;
+import edu.uog.course.courseData;
+import edu.uog.person.Person;
+import edu.uog.student.Student;
+import edu.uog.teacher.Teacher;
+
+public class TimeTableManagement {
+
+	public static void main(String[] args) {
+		courseData coursedata = new courseData();
+		roomData roomdata = new roomData();
+		
+		List<Student> students = new ArrayList<Student>();
+		
+		Person person = new Person();
+		Student student = new Student();
+		Teacher teacher = new Teacher();
+		Person p1 = new Person();
+		
+		Student s1 = new Student();
+		Teacher t1 = new Teacher();
+		
+		p1.setFORE_NAME("USAMA RAHEEM");
+		
+		s1.setPERSON_ID(p1);
+		s1.setSTUDENT_ROLLNO("19011519-001");
+		
+		t1.setPERSON_ID(p1);
+		t1.setEMPLOYEE_CODE("0041");
+		
+		System.out.println("Student Name: "+s1.getPERSON_ID().getFORE_NAME());
+		System.out.println("Teacher Name: "+t1.getPERSON_ID().getFORE_NAME());
+		
+
+//		s2 = s1;
+//		if (s1 == s2)
+//			System.out.println("Student Equal!");
+//		
+//		student.setFORE_NAME("USAMA MUSTAFA");
+//		student.setSTUDENT_ROLLNO("19011519-001");
+//		students.add(student);
+//		
+//		student = new Student();
+//		student.setFORE_NAME("USAMA RAHEEM");
+//		student.setSTUDENT_ROLLNO("19011519-002");
+//		students.add(student);
+//		
+//		student = new Student();
+//		student.setFORE_NAME("USAMA SAEED");
+//		student.setSTUDENT_ROLLNO("19011519-003");
+//		students.add(student);
+//
+//		student = new Student();
+//		student.setFORE_NAME("Jawad Altaf");
+//		student.setSTUDENT_ROLLNO("19011519-004");
+//		students.add(student);
+//
+//		for (int i=0;i<students.size();i++) {
+//			System.out.println(students.get(i).getSTUDENT_ROLLNO()+"\t"+students.get(i).getFORE_NAME());
+//		}
+//		
+//		person.setFORE_NAME("ABC");
+//
+//		teacher.setFORE_NAME("Dr. Nauman Riaz");
+//		teacher.setTEACHER_ID(1);
+//		
+	}
+}
